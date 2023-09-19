f20ce23d01bd61f812bcf2c7947a65c686eecd19
Mina51 <lahouijamina51@gmail.com>
2023-09-19 07:25:39 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -7,7 +7,8 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: formatted.
  * Return: print chars.
  */
-int _printf(const char *format, ...)
+
+int _printf(const char *format, ...);
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
